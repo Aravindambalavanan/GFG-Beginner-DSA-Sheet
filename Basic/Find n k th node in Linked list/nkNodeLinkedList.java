0ff639ae591582c45db740657ec633065ccd3842
Aravindambalavanan <j_aravind@outlook.com>
2023-09-11 13:59:16 +0530

    Added Java solution

---
+++ Basic/Find n k th node in Linked list/nkNodeLinkedList.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+class LinkedList {
+    static Node head;
+
+    public static void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+            temp.next = node;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            addToTheLast(head);
+            for (int i = 1; i < n; i++) {
+                int a = sc.nextInt();
+                addToTheLast(new Node(a));
+            }
+            int k = sc.nextInt();
+            GfG gfg = new GfG();
+            System.out.println(gfg.nknode(head, k));
+        }
+    }
+}
+
+class GfG {
+    public static int nknode(Node head, int k) {
+
+        if (head == null) {
+            return 0;
+        }
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            count++;
+        }
+        double n = Math.ceil((float) count / (float) k);
+        Node current = head;
+        for (int i = 0; i < (int) n - 1; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+}
